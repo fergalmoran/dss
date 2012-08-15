37d256aeab9984063aacd0775e2e4aa7ea8a2467
fergal.moran <fergal.moran@gmail.com>
2012-08-15 12:46:41 +0100

    Changed site name in initial_data

--- _working/release_audio.sql
+++ _working/release_audio.sql
@@ -1,6 +1,6 @@
-LOCK TABLES `www_releaseaudio` WRITE;
-/*!40000 ALTER TABLE `www_releaseaudio` DISABLE KEYS */;
-DELETE FROM `www_releaseaudio`;
-INSERT INTO `www_releaseaudio` VALUES (1,'release-audio/52d0c44f-db22-11e1-a030-0011b10a15ae/Late Nite Tuff Guy - One Night In the Disco.mp3',1,'Test release from someone or something..');
-/*!40000 ALTER TABLE `www_releaseaudio` ENABLE KEYS */;
+LOCK TABLES `spa_releaseaudio` WRITE;
+/*!40000 ALTER TABLE `spa_releaseaudio` DISABLE KEYS */;
+DELETE FROM `spa_releaseaudio`;
+INSERT INTO `spa_releaseaudio` VALUES (1,'release-audio/52d0c44f-db22-11e1-a030-0011b10a15ae/Late Nite Tuff Guy - One Night In the Disco.mp3',1,'Test release from someone or something..');
+/*!40000 ALTER TABLE `spa_releaseaudio` ENABLE KEYS */;
 UNLOCK TABLES;
