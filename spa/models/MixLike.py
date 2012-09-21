9e747c9ca21ea8b93b41fc46ab10099e32bb48b2
Fergal Moran <fergal.moran@gmail.com>
2012-09-21 10:15:30 +0100

    Changed display of Like in admin page

--- spa/models/MixLike.py
+++ spa/models/MixLike.py
@@ -3,3 +3,6 @@ from spa.models._Activity import _Activity
 
 class MixLike(_Activity):
     mix = models.ForeignKey('spa.Mix', related_name='likes')
+
+    def __unicode__(self):
+        return "%s on %s" % (self.user.get_full_name(), self.mix.title)
