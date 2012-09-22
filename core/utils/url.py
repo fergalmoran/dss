4415ac40f70bc5812f8493fa4a8472c48a284dc1
= <=>
2012-09-22 19:43:43 +0100

    Fixed mix add flow

--- core/utils/url.py
+++ core/utils/url.py
@@ -1,4 +1,7 @@
+import urlparse
+
 __author__ = 'fergalm'
 def urlclean(url):
-    ret = url.replace('//', '/')
+    #remove double slashes
+    ret = urlparse.urlparse(url).path.replace('//', '/')
     return ret
