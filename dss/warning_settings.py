43e7ae6f15782cf3f7f557b9650cbc5524893c80
Fergal Moran <fergal.moran@gmail.com>
2012-12-04 21:01:46 +0000

    Removed supression of override_urls warning as no longer being used.

--- dss/warning_settings.py
+++
@@ -1,6 +0,0 @@
-FILTER_WARNINGS = (
-    'ignore',
-    r"'override_urls' is a deprecated method & will be removed by v1.0.0. Please use ``prepend_urls`` instead.",
-    UserWarning,
-    r'tastypie.*'
-)
