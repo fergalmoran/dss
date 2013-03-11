d4ef08ef2970a98c06716c27993dc0f346f456fb
Fergal Moran <fergal.moran@gmail.com>
2013-03-11 15:05:47 +0000

    Fixed unicode for play

--- spa/models/MixPlay.py
+++ spa/models/MixPlay.py
@@ -6,7 +6,10 @@ class MixPlay(_Activity):
     mix = models.ForeignKey('spa.Mix', related_name='plays')
 
     def __unicode__(self):
-        return "%s %s %s %s" % (self.user.get_full_name, self.get_verb_passed(), self.get_object_name(), self.date)
+        if self.user is None:
+            return "%s %s %s %s" % ("Anonymous", self.get_verb_passed(), self.get_object_name(), self.date)
+        else:
+            return "%s %s %s %s" % (self.user.get_full_name, self.get_verb_passed(), self.get_object_name(), self.date)
 
     def get_verb_passed(self):
         return "played"
