302b965f0367b0f2adf5c160bab060d032c0efc8
Fergal Moran <fergal.moran@gmail.com>
2013-03-12 14:03:38 +0000

    Added migration for userfollows

--- spa/models/UserFollows.py
+++ spa/models/UserFollows.py
@@ -5,5 +5,5 @@ from spa.models._BaseModel import _BaseModel
 
 
 class UserFollows(_BaseModel):
-    user_from = models.OneToOneField(UserProfile)
-    user_to = models.OneToOneField(UserProfile)
+    user_from = models.OneToOneField(UserProfile, related_name='followers')
+    user_to = models.OneToOneField(UserProfile, related_name='following')
