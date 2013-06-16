dffab51dd63ed315240ab4584b2a9d89c50e845d
fergalmoran <fergal.moran@gmail.com>
2013-06-16 10:44:39 +0100

    Altered debug mode in javascript settings

--- templates/javascript/settings.js
+++ templates/javascript/settings.js
@@ -9,11 +9,11 @@ com.podnoms.settings = {
     liveStreamRoot: 'http://{{ LIVE_STREAM_URL }}:{{ LIVE_STREAM_PORT }}/{{ LIVE_STREAM_MOUNT }}',
     streamInfoUrl: 'http://{{ LIVE_STREAM_INFO_URL }}',
     volume: '{{ DEFAULT_AUDIO_VOLUME }}',
-    smDebugMode: '{{ SM_DEBUG_MODE }}',
-    isDebug: '{{ IS_DEBUG }}',
+    smDebugMode: {{ SM_DEBUG_MODE }},
+    isDebug: {{ IS_DEBUG }},
     drawTimelineOnMix: false,
     staticUrl: '{{ STATIC_URL }}',
-    urlArgs: '{{ IS_DEBUG }}' ? "" : "bust="+ (new Date()).getTime(),
+    urlArgs: {{ IS_DEBUG }} ? "" : "bust="+ (new Date()).getTime(),
     currentUser: {{ CURRENT_USER_ID }},
     /** simple helper to take an api JSON object and initialise a player item */
     setupPlayerWrapper: function (id, stream_url) {
