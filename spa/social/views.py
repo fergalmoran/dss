c51b60aae215b976dc7cd0f40df5951d164b3291
Fergal Moran <fergal.moran@gmail.com>
2013-07-09 10:49:19 +0100

    Altered image size in fb redirect

--- spa/social/views.py
+++ spa/social/views.py
@@ -38,7 +38,7 @@ def mix(request, args):
     except Mix.DoesNotExist:
         raise Http404
 
-    image = mix.get_image_url('320x320')
+    image = mix.get_image_url('1500x1500')
     audio_url = mix.get_stream_path()
     mix_url = mix.get_absolute_url()
     default = _getPayload(request)
