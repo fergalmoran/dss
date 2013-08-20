b914e89e90356460e2e5f10aa244e3628f22ff3a
Fergal Moran <fergal.moran@wasptech.com>
2013-08-20 12:06:03 +0100

    Fixed null pointer in js settings for anon user

--- spa/templates.py
+++ spa/templates.py
@@ -43,7 +43,7 @@ def get_dialog(request, dialog_name, **kwargs):
 
 def get_javascript(request, template_name):
     localsettings.JS_SETTINGS.update({
-        'CURRENT_USER_ID': request.user.get_profile().id or -1
+        'CURRENT_USER_ID': request.user.get_profile().id if not request.user.is_anonymous() else -1
     })
     return render_to_response(
         'javascript/%s.js' % template_name,
