6244947c99ba7ad026e5c8e0ac9eeb827294e43a
Fergal Moran <fergal.moran@wasptech.com>
2013-08-20 16:42:06 +0100

    Removed "mixes" from get_stream_url

--- spa/models/mix.py
+++ spa/models/mix.py
@@ -136,7 +136,7 @@ class Mix(_BaseModel):
         return super(Mix, self).get_image_url(self.mix_image, settings.STATIC_URL + 'img/default-track.png')
 
     def get_stream_path(self):
-        return '%s/mixes/%s.mp3' % (localsettings.STREAM_URL, self.uid)
+        return '%s/%s.mp3' % (localsettings.STREAM_URL, self.uid)
 
     def get_date_as_rfc822(self):
         return rfc822.formatdate(
