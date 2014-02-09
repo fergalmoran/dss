31968df4b3c901e9226b28c5cf9a0f6ecf59537e
Fergal Moran <fergal.moran@gmail.com>
2014-02-09 20:24:26 +0000

    Fixed orphan mix command

--- spa/management/commands/deleteorphanmp3.py
+++ spa/management/commands/deleteorphanmp3.py
@@ -8,17 +8,19 @@ from spa.models import Mix
 class Command(NoArgsCommand):
     def handle(self, *args, **options):
         try:
+            print "Starting"
             mixes_path = join(settings.MEDIA_ROOT, "mixes")
             expired_path = join(settings.MEDIA_ROOT, "mixes/expired")
             files = [f for f in listdir(mixes_path) if isfile(join(mixes_path, f))]
 
             for f in files:
+                print "Got a file: %s" % f
                 uid = os.path.splitext(f)[0]
                 try:
                     Mix.objects.get(uid=uid)
                 except Mix.DoesNotExist:
-                    new_file = os.path.join(expired_path, os.path.basename(f))
-                    os.rename(f, new_file)
+                    new_file = os.path.join(expired_path, f)
+                    os.rename(os.path.join(mixes_path, f), new_file)
                     print "Moved %s to %s" % (f, new_file)
         except Exception, ex:
             print "Error: %s" % ex.message
