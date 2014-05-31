cd3e0499ad743447d1110b1049b0174c4acb74af
Fergal Moran <fergal.moran@gmail.com>
2014-05-31 17:23:13 +0100

    Cleaned up ga code

--- core/analytics/google.py
+++ core/analytics/google.py
@@ -20,5 +20,6 @@ class ShowGoogleAnalyticsJS(template.Node):
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', '""" + str(code) + """', 'auto');
-ga('send', 'pageview');</script>"""
+ga('create', """ + str(code) + """, 'auto');
+ga('send', 'pageview');
+</script>"""
